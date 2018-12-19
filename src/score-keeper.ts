dba6102a139efd536bb4311fba5d90ccafc69b2e
DavidDupre <djdupre7@gmail.com>
2018-12-18 17:01:09 -0800

    Improve style point calculation

--- src/score-keeper.ts
+++ src/score-keeper.ts
@@ -3,9 +3,26 @@ import { StyleMeterConfig } from './config';
 
 
 /**
- * The period at which style degradation takes place
+ * The period at which style degradation takes place.
+ * This has no real effect on difficulty.
+ * Higher values update the meter more often but worsen performance.
  */
-const SCORE_TIMEOUT_MS = 500;
+const STYLE_DEGRADE_PERIOD_MS = 500;
+
+/**
+ * The rate of style degradation in points/s^2
+ */
+const STYLE_DEGRADE_ACC_PPS2 = 2;
+
+/**
+ * The max style point reward for content changes per event.
+ */
+const MAX_CHANGE_REWARD = 5;
+
+/**
+ * How many times more difficult it is to gain points at the highest rank than without any rank.
+ */
+const DIFFICULTY_FACTOR = 4;
 
 
 export interface ScoreChangeEvent {
@@ -32,17 +49,30 @@ export class ScoreKeeper {
     // timer used for style degradation
     private _timer: NodeJS.Timer;
 
+    /**
+     * The last time style points went up
+     */
+    private _lastUpdateTime = 0;
+
     private _disposables: vscode.Disposable[] = [
         this._onScoreChange,
         this._onRankChange
     ];
 
     constructor(public readonly config: StyleMeterConfig) {
-        // style degradation
-        // reduce the score at a constant rate
+        /**
+         * Style degradation.
+         * Reduce style points periodically at an amount proportional to inactivity.
+         */
         this._timer = setInterval(() => {
-            this._changeScore(-config.degradationFactor);
-        }, SCORE_TIMEOUT_MS);
+            let inactiveTime = new Date().valueOf() - this._lastUpdateTime;
+            if (inactiveTime > 100000) {
+                inactiveTime = 100000;
+            }
+            const acc_ppms2 = STYLE_DEGRADE_ACC_PPS2 / 1e6;
+            const penalty = inactiveTime * STYLE_DEGRADE_PERIOD_MS * acc_ppms2;
+            this._changeScore(-penalty * config.degradationFactor);
+        }, STYLE_DEGRADE_PERIOD_MS);
 
         vscode.workspace.onDidChangeTextDocument(this.onDidChangeTextDocument, this, this._disposables);
     }
@@ -57,7 +87,24 @@ export class ScoreKeeper {
 
     // increment score whenever they type
     public onDidChangeTextDocument(event: vscode.TextDocumentChangeEvent) {
-        this._changeScore(this.config.gainFactor);
+        /*
+        Reward changes proportional to the size of the change, up to a point.
+        This prevents copy-paste from being overpowered.
+        Also deleting will have no effect.
+        */
+        const changeSize = this._getChangeSize(event.contentChanges);
+        let reward = changeSize;
+        if (reward > MAX_CHANGE_REWARD) {
+            reward = MAX_CHANGE_REWARD;
+        }
+
+        // make it harder to earn points on higher ranks
+        const rankProgress = (this._rankIndex + 1) / this.config.ranks.length;
+        reward /= (rankProgress * (DIFFICULTY_FACTOR - 1) + 1);
+
+        this._changeScore(reward * this.config.gainFactor);
+
+        this._lastUpdateTime = new Date().valueOf();
     }
 
     public score(): number {
@@ -109,5 +156,13 @@ export class ScoreKeeper {
             }
         }
         return -1; // -1 represents no style ranking
+    }
+
+    private _getChangeSize(contentChanges: vscode.TextDocumentContentChangeEvent[]) {
+        let size = 0;
+        for (let change of contentChanges) {
+            size += change.text.length;
+        }
+        return size;
     }
 }
